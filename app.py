caf86a0f2b38b2a07563f3a5babd781a221736ac
w0px <152983879+w0px@users.noreply.github.com>
2023-12-09 15:55:19 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,26 +1,9 @@
 from flask import Flask, render_template, request
-import json
-
 
 app = Flask(__name__)
 
 # Initialize data as an empty dictionary
 data = {}
-
-# Your other routes and functions go here
-
-from flask import Flask, render_template, request, jsonify
-
-app = Flask(__name__)
-
-# Initialize data as an empty dictionary
-data = {}
-
-# Your other routes and functions go here
-
-from flask import Flask, request, jsonify
-
-app = Flask(__name__)
 
 # Your other routes and functions go here
 
@@ -31,22 +14,36 @@ def update_data():
         concatenated_data = request.form.get('payload')
 
         # Split the concatenated data into individual values
-        encounterCount, enemy_addr, item, lastShinyTime, species, spespc = map(int, concatenated_data.split(','))
+        highestAtkDef, highestSpeSpc, item, shinyvalue, species, spespc, atkdef = map(int, concatenated_data.split(','))
 
         # Do something with the data (e.g., store in a database, update variables, etc.)
+        attack = atkdef // 16
+        defense = atkdef % 16
+        speed = spespc // 16
+        special = spespc % 16
 
         # Update the 'data' dictionary
-        data['encounterCount'] = encounterCount
-        data['enemy_addr'] = enemy_addr
+        data['highestAtkDef'] = highestAtkDef
+        data['highestSpeSpc'] = highestSpeSpc
         data['item'] = item
-        data['lastShinyTime'] = lastShinyTime
+        data['shinyvalue'] = shinyvalue
         data['species'] = species
-        data['spespc'] = spespc
+        data['Attack'] = attack
+        data['Defense'] = defense
+        data['Speed'] = speed
+        data['Special'] = special
 
         print(data)  # This will print the updated data
 
         return 'Data received successfully'
-        
+    
+#route to handle the 'species' value
+@app.route('/species')
+def species():
+    # You can pass the 'species' data to the species.html template
+    species_value = data.get('species', '')
+    return render_template('species.html', data=data['species'])
+
 @app.route('/')
 def index():
     return render_template('index.html', data=data)

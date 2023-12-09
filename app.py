c63efd005a48e029d37184c979bd248c74892002
w0px <152983879+w0px@users.noreply.github.com>
2023-12-09 02:38:36 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,14 @@
+from flask import Flask, render_template, request
+import json
+
+
+app = Flask(__name__)
+
+# Initialize data as an empty dictionary
+data = {}
+
+# Your other routes and functions go here
+
 from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
@@ -7,34 +18,35 @@ data = {}
 
 # Your other routes and functions go here
 
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+# Your other routes and functions go here
+
 @app.route('/update_data', methods=['POST'])
 def update_data():
     if request.method == 'POST':
-        
-        received_data = request.get_json()
+        # Get the concatenated data from the request payload
+        concatenated_data = request.form.get('payload')
 
-        # Access individual values
-        encounterCount = received_data.get('encounterCount')
-        enemy_addr = received_data.get('enemy_addr')
-        item = received_data.get('item')
-        timeSinceLastShiny = received_data.get('timeSinceLastShiny')
-        species = received_data.get('species')
-        spespc = received_data.get('spespc')  # Add this line to get 'spespc'
+        # Split the concatenated data into individual values
+        encounterCount, enemy_addr, item, lastShinyTime, species, spespc = map(int, concatenated_data.split(','))
 
         # Do something with the data (e.g., store in a database, update variables, etc.)
 
         # Update the 'data' dictionary
-        data['encounterCount'] = encounterCount  # Use the value received from Lua
+        data['encounterCount'] = encounterCount
         data['enemy_addr'] = enemy_addr
         data['item'] = item
-        data['timeSinceLastShiny'] = timeSinceLastShiny
+        data['lastShinyTime'] = lastShinyTime
         data['species'] = species
         data['spespc'] = spespc
 
         print(data)  # This will print the updated data
 
         return 'Data received successfully'
-
+        
 @app.route('/')
 def index():
     return render_template('index.html', data=data)

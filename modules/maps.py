04225b3856ae7cec449e3595369103b7b92e88d6
w0px <152983879+w0px@users.noreply.github.com>
2024-02-07 14:28:26 +0100

    Add files via upload

---
+++ modules/maps.py
@@ -0,0 +1,11 @@
+map_names = {
+    "8.6": "Route 33",
+    "24.4": "New Bark Town",
+    "24.3": "Route 29",
+    "5.9": "Route 46",
+    "26.1": "Route 30",
+    "26.2": "Route 31",
+    "3.78": "Dark Cave (Violet Entrance)",
+    "10.3": "Route 36",
+    "3.27": "Ruins of Alph"
+}
